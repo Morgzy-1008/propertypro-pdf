aa14ea4823e35c35186f40bb97db94c14e5db83c
Morgzy-1008 <morgzy@hudsonhomes.com.au>
2026-08-23 12:40:17 +1000

    fix: Deploy all 33 crystal-clear photorealistic AI outpainted widescreen double-storey facades

--- src/components/flyer/preRenderedFacades.data.ts
+++ src/components/flyer/preRenderedFacades.data.ts
@@ -1,14 +1,98 @@
 export const PRE_RENDERED_FACADES: Record<string, string> = {
-  // Widescreen 2400x937 high-res outpainted double-storey facades
+  // All 33 high-resolution AI outpainted widescreen double-storey facades (2400x937, 210:82 aspect ratio)
   allure: "/facades/allure_widescreen.jpg",
   "allure-2-stry": "/facades/allure_widescreen.jpg",
   "allure-facade": "/facades/allure_widescreen.jpg",
+
+  ascot: "/facades/ascot_widescreen.jpg",
+  "ascot-facade-h2-double-storey": "/facades/ascot_widescreen.jpg",
+  "ascot-facade": "/facades/ascot_widescreen.jpg",
 
   ashton: "/facades/ashton_widescreen.jpg",
   "ashton-2-stry": "/facades/ashton_widescreen.jpg",
   "ashton-facade": "/facades/ashton_widescreen.jpg",
 
-  ascot: "/facades/ascot_widescreen.jpg",
-  "ascot-facade-h2-double-storey": "/facades/ascot_widescreen.jpg",
-  "ascot-facade": "/facades/ascot_widescreen.jpg",
+  cambridge: "/facades/cambridge_widescreen.jpg",
+  "cambridge-2-stry": "/facades/cambridge_widescreen.jpg",
+  "cambridge-facade": "/facades/cambridge_widescreen.jpg",
+
+  centro: "/facades/centro_widescreen.jpg",
+  "centro-facade-h2-double-storey": "/facades/centro_widescreen.jpg",
+  "centro-facade": "/facades/centro_widescreen.jpg",
+
+  "classic-double-garage": "/facades/classic-double-garage_widescreen.jpg",
+  "classic-plus-double-garage": "/facades/classic-plus-double-garage_widescreen.jpg",
+
+  como: "/facades/como_widescreen.jpg",
+  "como-facade-h2-double-storey": "/facades/como_widescreen.jpg",
+  "como-facade": "/facades/como_widescreen.jpg",
+
+  contemporary: "/facades/contemporary_widescreen.jpg",
+  "contemporary-2-stry": "/facades/contemporary_widescreen.jpg",
+
+  "deco-double-garage": "/facades/deco-double-garage_widescreen.jpg",
+  "deco-single-garage": "/facades/deco-single-garage_widescreen.jpg",
+
+  delta: "/facades/delta_widescreen.jpg",
+  "delta-facade-h2-double-storey": "/facades/delta_widescreen.jpg",
+
+  deluxe: "/facades/deluxe_widescreen.jpg",
+  "deluxe-2-stry": "/facades/deluxe_widescreen.jpg",
+
+  flair: "/facades/flair_widescreen.jpg",
+  "flair-facade-h2-double-storey": "/facades/flair_widescreen.jpg",
+
+  grande: "/facades/grande_widescreen.jpg",
+  "grande-2-stry": "/facades/grande_widescreen.jpg",
+
+  madison: "/facades/madison_widescreen.jpg",
+  "madison-2-stry": "/facades/madison_widescreen.jpg",
+
+  majestic: "/facades/majestic_widescreen.jpg",
+  "majestic-facade-double-garage2-stry": "/facades/majestic_widescreen.jpg",
+
+  "mantra-double-garage": "/facades/mantra-double-garage_widescreen.jpg",
+  "mantra-single-garage": "/facades/mantra-single-garage_widescreen.jpg",
+
+  marina: "/facades/marina_widescreen.jpg",
+  "marina-2-stry": "/facades/marina_widescreen.jpg",
+
+  meridian: "/facades/meridian_widescreen.jpg",
+  "meridian-2-stry": "/facades/meridian_widescreen.jpg",
+
+  monash: "/facades/monash_widescreen.jpg",
+  "monash-2-stry": "/facades/monash_widescreen.jpg",
+
+  mondo: "/facades/mondo_widescreen.jpg",
+  "mondo-2-stry": "/facades/mondo_widescreen.jpg",
+
+  novare: "/facades/novare_widescreen.jpg",
+  "novare-2-stry": "/facades/novare_widescreen.jpg",
+
+  oxford: "/facades/oxford_widescreen.jpg",
+  "oxford-2-stry": "/facades/oxford_widescreen.jpg",
+
+  riviera: "/facades/riviera_widescreen.jpg",
+  "riviera-2-stry": "/facades/riviera_widescreen.jpg",
+
+  royale: "/facades/royale_widescreen.jpg",
+  "royale-2-stry": "/facades/royale_widescreen.jpg",
+
+  saville: "/facades/saville_widescreen.jpg",
+  "saville-2-stry": "/facades/saville_widescreen.jpg",
+
+  sierra: "/facades/sierra_widescreen.jpg",
+  "sierra-2-stry": "/facades/sierra_widescreen.jpg",
+
+  soho: "/facades/soho_widescreen.jpg",
+  "soho-facade-h2-double-storey": "/facades/soho_widescreen.jpg",
+
+  tempo: "/facades/tempo_widescreen.jpg",
+  "tempo-facade-h2-double-storey": "/facades/tempo_widescreen.jpg",
+
+  vista: "/facades/vista_widescreen.jpg",
+  "vista-2-stry": "/facades/vista_widescreen.jpg",
+
+  windsor: "/facades/windsor_widescreen.jpg",
+  "windsor-2-stry": "/facades/windsor_widescreen.jpg",
 };
